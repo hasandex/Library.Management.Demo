b015cef317cb195c07355fffaf29ca95674e05a5
hasan <hasankassab375@gmail.com>
2025-12-06 19:25:35 +0400

    Delete AvailableEditions column frm Book table

---
+++ Library.Management.Demo/Migrations/20251206152226_RemoveColumn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Management.Demo.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE Book DROP COLUMN AvailableEditions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
