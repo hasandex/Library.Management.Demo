e492972815fa10154c2930b8e0b655ca1e89ea49
hasan <hasankassab375@gmail.com>
2025-12-15 15:34:46 +0400

    Create Adminitration Controller to manage the Identity operations, and create endpoint to add roles

---
+++ Library.Management.Demo/Controllers/AdministrationController.cs
@@ -0,0 +1,33 @@
+﻿using Library.Management.Demo.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Library.Management.Demo.Controllers
+{
+    [ApiController]
+    public class AdministrationController : ControllerBase
+    {
+        private readonly RoleService _roleService;
+        public AdministrationController(RoleService roleService)
+        {
+            _roleService = roleService;
+        }
+        [HttpPost("CreateRole")]
+        public async Task<IActionResult> CreateRole(string roleName)
+        {
+            try
+            {
+                var result = await _roleService.CreateRole(roleName);
+                if (result.Succeeded)
+                {
+                    return Created();
+                }
+                return BadRequest(result.Errors);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
