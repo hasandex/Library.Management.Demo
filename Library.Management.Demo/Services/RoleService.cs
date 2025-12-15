bf7895545b5d85128f360950f84ca114ba0e360d
hasan <hasankassab375@gmail.com>
2025-12-15 15:32:56 +0400

    Add Role Service to create a role

---
+++ Library.Management.Demo/Services/RoleService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Library.Management.Demo.Services
+{
+    public class RoleService
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleService(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<IdentityResult> CreateRole(string roleName)
+        {
+            var roleExist = await _roleManager.RoleExistsAsync(roleName);
+            if (!roleExist)
+            {
+                return await _roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+            return IdentityResult.Failed(new IdentityError() { Description = "Role already exists" });
+        }
+    }
+}
